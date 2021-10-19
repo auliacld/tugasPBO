34225f967ca091451c365f8b9849b3ea9c624a25
Aulia <auliaclaudia@upi.edu>
2021-10-19 21:41:02 +0700

    tugas

--- Latihan Django/sik/sik/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def halo(request):
-	return HttpResponse("Haii")
--- Latihan Django/sik/sik2020/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Sik2020Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sik2020'
